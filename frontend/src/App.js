b789a53111086458da3fbf5566f94c5e11ab99bb
Shivanisachan6690 <shivi.ss1815@gmail.com>
2022-12-10 12:04:27 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,12 @@ import AdminProfile from "./components/admin/Profile";
 import UserProfile from "./components/user/Profile";
 import Header from "./components/main/Header";
 import Main from "./components/main";
+import User from "./components/user"
 import Home from "./components/main/Home";
 import ManageUser from "./components/admin/ManageUser";
 import FileManager from "./components/user/FileManager";
+import AudioBook from "./components/user/AudioBook";
+
 
 function App() {
   return (
@@ -21,13 +24,15 @@ function App() {
             <Route path="signup" element={<Signup />} />
             <Route path="manageuser" element={<ManageUser/>}/>
             <Route path="filemanager" element={<FileManager/>}/>
+            
           </Route>
 
           {/* <Route element={<Admin />} path="admin" /> */}
           <Route path="profile" element={<AdminProfile />} />
 
-          {/* <Route element={<User />} path="user" /> */}
+          <Route element={<User />} path="user" />
           <Route path="profile" element={<UserProfile />} />
+          <Route path="audiobook" element={<AudioBook/>}/>
         </Routes>
       </BrowserRouter>
     </div>
