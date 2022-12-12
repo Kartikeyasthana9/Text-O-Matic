cc70deb1941337e44821d5145b95d29614ca79d0
Kartikeyasthana9 <harshitasthana9@gmail.com>
2022-12-12 12:49:30 +0530

    xyz

--- frontend/src/components/user/Summarizer.js
+++ frontend/src/components/user/Summarizer.js
@@ -4,7 +4,6 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Fab from "@mui/material/Fab";
 
-
 const Summarizer = () => {
   return (
     <div>
@@ -54,7 +53,7 @@ const Summarizer = () => {
                 className="card w-80 animate__animated animate__slideInUp "
                 style={{ background: "#dff1ee" }}
               >
-                <div className="card-body" style={{transition:"1sec" }}>
+                <div className="card-body" style={{ transition: "1sec" }}>
                   <img
                     src="https://www.sammurize.com/images/assets/ils_15.svg"
                     alt=""
@@ -116,7 +115,10 @@ const Summarizer = () => {
         <div className="container ">
           <div className="card">
             <div className="card-body">
-              <p className="display-5 text-center fw bold animate__animated animate__fadeInBottomLeft"> Summarizer</p>
+              <p className="display-5 text-center fw bold animate__animated animate__fadeInBottomLeft">
+                {" "}
+                Summarizer
+              </p>
               <hr />
 
               <TextField
@@ -146,6 +148,215 @@ const Summarizer = () => {
           </div>
         </div>
       </div>
+
+      {/* //////////////////////////////////////////////// */}
+
+      <>
+        {/* Footer */}
+        <footer className="bg-secondary text-center text-white">
+          {/* Grid container */}
+          <div className="container p-4">
+            {/* Section: Social media */}
+            <section className="mb-4">
+              {/* Facebook */}
+              <a
+                className="btn btn-outline-light btn-floating m-1"
+                href="#!"
+                role="button"
+              >
+                <i className="fab fa-facebook-f" />
+              </a>
+              {/* Twitter */}
+              <a
+                className="btn btn-outline-light btn-floating m-1"
+                href="#!"
+                role="button"
+              >
+                <i className="fab fa-twitter" />
+              </a>
+              {/* Google */}
+              <a
+                className="btn btn-outline-light btn-floating m-1"
+                href="#!"
+                role="button"
+              >
+                <i className="fab fa-google" />
+              </a>
+              {/* Instagram */}
+              <a
+                className="btn btn-outline-light btn-floating m-1"
+                href="#!"
+                role="button"
+              >
+                <i className="fab fa-instagram" />
+              </a>
+              {/* Linkedin */}
+              <a
+                className="btn btn-outline-light btn-floating m-1"
+                href="#!"
+                role="button"
+              >
+                <i className="fab fa-linkedin-in" />
+              </a>
+              {/* Github */}
+              <a
+                className="btn btn-outline-light btn-floating m-1"
+                href="#!"
+                role="button"
+              >
+                <i className="fab fa-github" />
+              </a>
+            </section>
+            {/* Section: Social media */}
+            {/* Section: Form */}
+            <section className="">
+              <form action="">
+                {/*Grid row*/}
+                <div className="row d-flex justify-content-center">
+                  {/*Grid column*/}
+                  <div className="col-auto">
+                    <p className="pt-2">
+                      <strong>Sign up for our newsletter</strong>
+                    </p>
+                  </div>
+                  {/*Grid column*/}
+                  {/*Grid column*/}
+                  <div className="col-md-5 col-12">
+                    {/* Email input */}
+                    <div className="form-outline form-white mb-4">
+                      <input
+                        type="email"
+                        id="form5Example21"
+                        className="form-control"
+                      />
+                      <label className="form-label" htmlFor="form5Example21">
+                        Email address
+                      </label>
+                    </div>
+                  </div>
+                  {/*Grid column*/}
+                  {/*Grid column*/}
+                  <div className="col-auto">
+                    {/* Submit button */}
+                    <button
+                      type="submit"
+                      className="btn btn-outline-light mb-4"
+                    >
+                      Subscribe
+                    </button>
+                  </div>
+                  {/*Grid column*/}
+                </div>
+                {/*Grid row*/}
+              </form>
+            </section>
+            {/* Section: Form */}
+            {/* Section: Text */}
+            <section className="mb-4">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                distinctio earum repellat quaerat voluptatibus placeat nam,
+                commodi optio pariatur est quia magnam eum harum corrupti dicta,
+                aliquam sequi voluptate quas.
+              </p>
+            </section>
+            {/* Section: Text */}
+            {/* Section: Links */}
+            <section className="">
+              {/*Grid row*/}
+              <div className="row">
+                {/*Grid column*/}
+                <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                  <h5 className="text-uppercase">Links</h5>
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <a href="#!" className="text-white">
+                        Link 1
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" className="text-white">
+                        Link 2
+                      </a>
+                    </li>
+                   
+                  </ul>
+                </div>
+                {/*Grid column*/}
+                {/*Grid column*/}
+                <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                  <h5 className="text-uppercase">Links</h5>
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <a href="#!" className="text-white">
+                        Link 1
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" className="text-white">
+                        Link 2
+                      </a>
+                    </li>
+                  
+                  </ul>
+                </div>
+                {/*Grid column*/}
+                {/*Grid column*/}
+                <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                  <h5 className="text-uppercase">Links</h5>
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <a href="#!" className="text-white">
+                        Link 1
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" className="text-white">
+                        Link 2
+                      </a>
+                    </li>
+                   
+                  </ul>
+                </div>
+                {/*Grid column*/}
+                {/*Grid column*/}
+                <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                  <h5 className="text-uppercase">Links</h5>
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <a href="#!" className="text-white">
+                        Link 1
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" className="text-white">
+                        Link 2
+                      </a>
+                    </li>
+                    
+                  </ul>
+                </div>
+                {/*Grid column*/}
+              </div>
+              {/*Grid row*/}
+            </section>
+            {/* Section: Links */}
+          </div>
+          {/* Grid container */}
+          {/* Copyright */}
+          <div
+            className="text-center p-3"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}
+          >
+            © 2022 Copyright:
+            <a className="text-white" href="https://mdbootstrap.com/">
+              Text-o-matic.com
+            </a>
+          </div>
+          {/* Copyright */}
+        </footer>
+        {/* Footer */}
+      </>
     </div>
   );
 };
