8f5c1c7b1373289c88183986c3d8b486e92875ec
m-triple-m <triplem656@mail.com>
2023-01-16 13:50:11 +0530

    ..

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,8 @@ const mySchema = new Schema({
     email: String,
     password: String,
     contact: String,
+    avatar : String,
+    isAdmin: {type : Boolean, default: false}
 })
 
 module.exports = model('userCollection', mySchema);
