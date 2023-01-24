3ccf19ac5247c3c1036593941bc33f0f56345a00
sufiya <ansarisufiya507@gmail.com>
2023-01-24 14:23:32 +0530

    abc.......

--- Python API/API/majorapi.py
+++ Python API/API/majorapi.py
@@ -1,7 +1,14 @@
 # import fastapi
 from fastapi import FastAPI, Form, File, UploadFile
 import summarization as smz
+import os
 app = FastAPI()
+
+def save_file(file, path):
+    print(os.getcwd())
+    with open(os.path.join(path, file.filename), "wb") as buffer:
+        buffer.write(file.file.read())
+
 
 # Create a route
 @app.get("/")
@@ -16,26 +23,25 @@ async def create_file(file: bytes = File(...)):
 # Create a route that excepts files from form
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+    save_file(file, path="../uploads")
+    return {"filename": file.filename, 'status': 'success'}
 
 # Create a route that excepts music files
 @app.post("/uploadmusicfile/")
 async def create_upload_music(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+    save_file(file, path="../uploads/music")
+    return {"filename": file.filename, 'status': 'success'}
 
 # create a api route that summarizes the text str 
 @app.post("/textomatic/api/v1/summarize/text")
 async def summarize_text(text: str = Form(...)):
-    try:
+    
         with open('temp.txt', 'w') as f:
             f.write(text)
         summary = smz.generate_summary('temp.txt')
         return {'summary': summary,
                 'summary_length': len(summary),
                 'status': 'success'}
-    except Exception as e:
-        return {'error': str(e), 'status': 'failed'}
-
 
 # create an api route that summarizes the file after getting text from the file
 @app.post("/textomatic/api/v1/summarize")
--- frontend/src/components/user/Header.js
+++ frontend/src/components/user/Header.js
@@ -43,7 +43,89 @@ const Header = () => {
               </li>
             </ul>
             {/* Left links */}
+            
+            
           </div>
+          <ul className="navbar-nav ms-auto d-flex flex-row">
+          {/* Notification dropdown */}
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link me-3 me-lg-0 dropdown-toggle hidden-arrow"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-mdb-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <i className="fas fa-bell" />
+              <span className="badge rounded-pill badge-notification bg-danger">
+                1
+              </span>
+            </a>
+            <ul
+              className="dropdown-menu dropdown-menu-end"
+              aria-labelledby="navbarDropdownMenuLink"
+            >
+              <li>
+                <a className="dropdown-item" href="#">
+                  Some news
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Another news
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </li>
+          {/* Icon dropdown */}
+        
+          {/* Avatar */}
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle hidden-arrow d-flex align-items-center"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-mdb-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img
+                src="https://mdbootstrap.com/img/new/avatars/2.jpg"
+                className="rounded-circle"
+                height={35}
+                alt="Avatar"
+                loading="lazy"
+              />
+            </a>
+            <ul
+              className="dropdown-menu dropdown-menu-end"
+              aria-labelledby="navbarDropdownMenuLink"
+            >
+              <li>
+                <a className="dropdown-item" href="/user/UserProfile">
+                  My profile
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Settings
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+
                       
           {/* Collapsible wrapper */}
         </div>
